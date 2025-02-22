9b9fa536a821b7b5bde2baf12961dc6ccbb076a5
burak_cokkececi <98846167+MRCERYANCI@users.noreply.github.com>
2025-02-23 02:36:13 +0300

    ...

--- OnlineEdu.PresentationLayer/ViewComponents/HomePageViewComponents/_HomePageSliderAreaComponentPartial.cs
+++ OnlineEdu.PresentationLayer/ViewComponents/HomePageViewComponents/_HomePageSliderAreaComponentPartial.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.DtoLayer.Dtos.BannerDtos;
+using OnlineEdu.PresentationLayer.Helpers;
+using System.Net.Http;
 
 namespace OnlineEdu.PresentationLayer.ViewComponents.HomePageViewComponents
 {
     public class _HomePageSliderAreaComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly HttpClient _httpClientFactory = HttpClientInstance.CreateClient();
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var values = await _httpClientFactory.GetFromJsonAsync<List<ResultBannerDto>>("Banners");
+            return View(values);
         }
     }
 }
